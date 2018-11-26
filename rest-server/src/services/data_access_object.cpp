9bfb8f78277ecce160583df38c35b4958e316204
asumike <mmccull02@yahoo.com>
2018-11-26 13:21:31 -0800

    This branch (fea-88) was just previously rebased onto dev by Kris and I. As such, there were are few changes performed on branch fea-88-merge that were not on fea-88. Note: fea-88-merge had been branched off of dev and its files manually updated with changes from the fea-88 branch, in attempt to merge into dev without having to do a rebase. I should have done a rebase initially, but now our history is correct, and with these last few changes, up to date with all tasks from fea 88.

--- rest-server/src/services/data_access_object.cpp
+++ rest-server/src/services/data_access_object.cpp
@@ -10,23 +10,26 @@ DataAccessObject& DataAccessObject::Instance() {
     static DataAccessObject instance;
     return instance;
 }
-
 void DataAccessObject::SetConnectionString(std::string connStr) {
     connStr_ = connStr;
 }
-
 std::vector<User> DataAccessObject::GetUsers() {
-    nanodbc::connection connection(connStr_);
-    nanodbc::result results;
-    results = execute(connection, NANODBC_TEXT("select username, userId, password from user;"));
-    std::vector<User> db_users;
-    while (results.next()) {
-        db_users.push_back(User {
-            results.get<std::string>(0, "null_user")
-            , results.get<int>(1, 0)
-            , results.get<std::string>(2, "null_pw")});
+    try {
+        nanodbc::connection connection(connStr_);
+        nanodbc::result results;
+        results = execute(connection, NANODBC_TEXT("select username, userId, password from user;"));
+        std::vector<User> db_users;
+        while (results.next()) {
+            db_users.push_back(User {
+                results.get<std::string>(0, "null_user")
+                , results.get<int>(1, 0)
+                , results.get<std::string>(2, "null_pw")});
+        }
+        return db_users;
+    } catch (...) {
+        std::vector<User> db_users;
+        return db_users;
     }
-    return db_users;
 }
 /**
  * Gets user by partial string match of name, ignores case.
@@ -64,43 +67,60 @@ std::vector<User> DataAccessObject::GetUserByID(int id) {
     return matching_users;
 }
 bool DataAccessObject::RemoveUser(User user) {
-    nanodbc::connection connection(connStr_);
-    nanodbc::statement statement(connection);
-    prepare(statement, NANODBC_TEXT("delete from user where userId =?;"));
-    statement.bind(0, &user.id);
-    execute(statement);
-    return true;
+    try {
+        nanodbc::connection connection(connStr_);
+        nanodbc::statement statement(connection);
+        prepare(statement, NANODBC_TEXT("delete from user where userId =?;"));
+        statement.bind(0, &user.id);
+        execute(statement);
+        return true;
+    } catch (...) {
+        return false;
+    }
 }
 void DataAccessObject::AddUser(User user) {
-    nanodbc::connection connection(connStr_);
-    nanodbc::statement statement(connection);
-    prepare(statement, NANODBC_TEXT("insert into user (username, password) values(?,?);"));
-    nanodbc::string const username = NANODBC_TEXT(user.name);
-    statement.bind(0, username.c_str());
-    nanodbc::string const password = NANODBC_TEXT(user.password);
-    statement.bind(1, password.c_str());
-    execute(statement);
-    // Get the ID of the newly created user record.
-    // Note: LAST_INSERT_ID() is per connection.
-    nanodbc::result results;
-    results = execute(connection, NANODBC_TEXT("SELECT LAST_INSERT_ID();"));
-    int newId = results.get<int>(0, 0);
-    // Update the user.id if successful.
-    if (results.next() && newId > 0) {
-      user.id = newId;
+    try {
+        nanodbc::connection connection(connStr_);
+        nanodbc::statement statement(connection);
+        prepare(statement, NANODBC_TEXT("insert into user (username, password) values(?,?);"));
+        nanodbc::string const username = NANODBC_TEXT(user.name);
+        statement.bind(0, username.c_str());
+        nanodbc::string const password = NANODBC_TEXT(user.password);
+        statement.bind(1, password.c_str());
+        execute(statement);
+        // Get the ID of the newly created user record.
+        // Note: LAST_INSERT_ID() is per connection.
+        nanodbc::result results;
+        results = execute(connection, NANODBC_TEXT("SELECT LAST_INSERT_ID();"));
+        int newId = results.get<int>(0, 0);
+        // Update the user.id if successful.
+        if (results.next() && newId > 0) {
+            user.id = newId;
+        }
+    } catch (...) {
+        // TODO(All): Something?
     }
 }
 void DataAccessObject::UpdateUser(int id, web::json::object user_info) {
-    for (auto& user : users__) {
-        if (user.id == id) {
-            for (auto iter = user_info.cbegin(); iter != user_info.cend(); ++iter) {
-                auto &key = iter->first;
-                auto &value = iter->second;
-                if (key.compare("name") == 0) user.name = value.as_string();
-                else if (key.compare("password") == 0) user.password = value.as_string();
-            }
-            break;
+    try {
+        // Declare and set nanodbc statement parameters.
+        nanodbc::string username;
+        nanodbc::string password;
+        for (auto iter = user_info.cbegin(); iter != user_info.cend(); ++iter) {
+            auto &key = iter->first;
+            auto &value = iter->second;
+            if (key.compare("name") == 0) username = NANODBC_TEXT(value.as_string());
+            else if (key.compare("password") == 0) password = NANODBC_TEXT(value.as_string());
         }
+        nanodbc::connection connection(connStr_);
+        nanodbc::statement statement(connection);
+        prepare(statement, NANODBC_TEXT("update user set username = ?, password = ? where userId = ?;"));
+        statement.bind(0, username.c_str());
+        statement.bind(1, password.c_str());
+        statement.bind(2, &id);
+        execute(statement);
+    } catch (...) {
+        // TODO(All): Something?
     }
 }
 }  // namespace cadg_rest
