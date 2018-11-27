655c494f6bca5c7d556c2ccf2da0b82ddba36a41
Shawn Hulce <shawn_hulce@hotmail.com>
2018-11-26 22:14:45 -0600

    Progress push for evening.

---
+++ rest-server/src/soap/controller/soap_controller.hpp
@@ -0,0 +1,21 @@
+// Copyright 2018   Vaniya Agrawal, Ross Arcemont, Kristofer Hoadley,
+//                  Shawn Hulce, Michael McCulley
+// Created by Shawn on 11/26/2018.
+//
+
+#ifndef SOAP_CONTROLLER_HPP
+#define SOAP_CONTROLLER_HPP
+#include <cadg_rest::controller.hpp>
+
+
+namespace cadg_soap {
+class SoapController: public cadg_rest::Controller {
+
+    public:
+
+    private:
+
+};
+
+} //end cadg_soap
+#endif //CADG_REST_SERVER_SOAP_CONTROLLER_HPP
