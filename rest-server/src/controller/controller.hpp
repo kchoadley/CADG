e67f30a0f4cf8e2aaa7e9c3d127f236104eb6c79
Kristofer Hoadley <kristoferhoadley@gmail.com>
2018-11-28 11:00:53 -0600

    Documentation for Rest Server Controller classes.

--- rest-server/src/controller/controller.hpp
+++ rest-server/src/controller/controller.hpp
@@ -1,5 +1,18 @@
 // Copyright 2018   Vaniya Agrawal, Ross Arcemont, Kristofer Hoadley,
 //                  Shawn Hulce, Michael McCulley
+/// A REST endpoint controller abstract class.
+/**
+ * Controller is a base class for REST controller classes, 
+ * it has some common methods defined and leaves methods that
+ * need to be defined by parent classes undefined. A controller
+ * has an endpoint, can be started with Accept and ended with 
+ * Shutdown. The default supported http verbs are GET, PUT, POST,
+ * and DELETE.
+ *
+ * @file        controller.cpp
+ * @authors     Kristofer Hoadley
+ * @date        November, 2018
+ */
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 #include <map>
@@ -14,15 +27,41 @@ using namespace http;
 using namespace http::experimental::listener;
 
 namespace cadg_rest {
+/**
+ * A REST endpoint controller abstract class.
+ * 
+ * Controller is a base class for REST controller classes, 
+ * it has some common methods defined and leaves methods that
+ * need to be defined by parent classes undefined. A controller
+ * has an endpoint, can be started with Accept and ended with 
+ * Shutdown. The default supported http verbs are GET, PUT, POST,
+ * and DELETE.
+ * Uses a Logger with LogNetworkActivity for logging.
+ */
 class Controller {
   public: 
+    // Takes in a Logger object reference that adheres to the Logger interface.
     Controller(LoggerInterface& logger__) : logger__(logger__) { }
     ~Controller() { }
 
     void endpoint(const std::string& endpoint);
     std::string endpoint() const;
+    /**
+     * Starts the controller listening at it's endpoint.
+     * The endpoint should be set first.
+     */
     pplx::task<void> Accept();
+    /// Stops the controller from listening at it's endoint.
     pplx::task<void> Shutdown();
+    /**
+     * Queries parses the provided query string of a URL path.
+     * 
+     * The queries are parsed with '&' delimiting queries
+     * and with '=' delimiting values from keys.
+     * 
+     * @param query_string The path query string to be parsed.
+     * @return A <string, string> map of query name as keys and variable as value.
+     */
     static std::map<std::string, std::string> Queries(std::string query_string);
 
     virtual void InitHandlers() = 0;
@@ -33,6 +72,7 @@ class Controller {
   protected:
     http_listener listener__;
     LoggerInterface& logger__;
+    /// The default response to return if the http verb is not implemented.
     static json::value ResponseNotImpl(const http::method& method);
 };
 }
