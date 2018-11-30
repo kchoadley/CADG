4a1eb06702f088a83303fa3c172c94d2ce9778bc
Kristofer Hoadley <kristoferhoadley@gmail.com>
2018-11-30 12:13:32 -0600

    Corrected path for test xml input

--- test/soap/xml-parsing/xml_parser_test.cpp
+++ test/soap/xml-parsing/xml_parser_test.cpp
@@ -9,7 +9,7 @@
 #include "xml_parser.hpp"
 
 TEST(xml_parse_tests, import) {
-    std::ifstream input("soap_message_test.xml");
+    std::ifstream input("../soap/xml-parsing/soap_message_test.xml");
     CAPMessage test_message = ReadXML(input);
     EXPECT_EQ(test_message.identifier, "KSTO1055887203");
     EXPECT_EQ(test_message.sender, "KSTO@NWS.NOAA.GOV");
