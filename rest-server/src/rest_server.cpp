e019c4b39acb972f77e58a81fcffdaae3caf81ae
asumike <mmccull02@yahoo.com>
2018-12-01 18:41:41 -0800

    Removed old TODO comment

--- rest-server/src/rest_server.cpp
+++ rest-server/src/rest_server.cpp
@@ -16,7 +16,6 @@ int main(int argc, const char * argv[]) {
     LoggerInterface& logger(Logger::Instance());
     logger.LogLevel(LogLevel::DEBUG);
     logger.Log(LogLevel::INFO, "Starting cadg rest server");
-    // TODO(Mike): Create database and update connection string.
     DataAccessObject::Instance().SetConnectionString(
         "Driver={MySQL8};Server=cadg-db;Port=3306;Database=admin_db;Uid=root;Pwd=example;");
     UserController user_controller(Logger::Instance(), DataAccessObject::Instance());
