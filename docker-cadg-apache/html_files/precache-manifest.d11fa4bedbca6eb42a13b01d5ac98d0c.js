f5b5ddb3abe2d8f5549841a8e8bc7c2f49c652d6
vsagrawal0 <vaniya0@yahoo.com>
2019-01-11 20:01:21 -0600

    Create mini-bundle of React project and deploy to apache server for hosting.

---
+++ docker-cadg-apache/html_files/precache-manifest.d11fa4bedbca6eb42a13b01d5ac98d0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05d805f3bea0064f442f",
+    "url": "/static/js/main.05d805f3.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "05d805f3bea0064f442f",
+    "url": "/static/css/main.f6ad6b44.chunk.css"
+  },
+  {
+    "revision": "d46e2dc88697cd242886a071a3dfbabd",
+    "url": "/index.html"
+  }
+];
