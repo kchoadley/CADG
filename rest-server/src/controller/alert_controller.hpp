f51d452d225370e70eeb743ea669cbe1ef1177de
vsagrawal0 <vaniya0@yahoo.com>
2019-01-21 18:21:48 -0600

    Align endpoint with REST API spec and document methods.

--- rest-server/src/controller/alert_controller.hpp
+++ rest-server/src/controller/alert_controller.hpp
@@ -1,7 +1,9 @@
 /// A REST endpoint controller implementation class for alerts
 /**
  * AlertController implements the interface defined by the Controller class.
- * This represents an endpoint in the Rest Server for receiving data about alerts.
+ * This represents an endpoint in the Rest Server for receiving and sending
+ * data about alerts. Alert data can only be sent and received - not updated
+ * or deleted.
  *
  * Copyright 2018   Vaniya Agrawal, Ross Arcemont, Kristofer Hoadley,
  *                  Shawn Hulce, Michael McCulley
@@ -23,10 +25,27 @@ public:
     AlertController(AlertController& logger__, DataAccessInterface& dao__) : dao__(dao__), Controller(logger__) {}
     ~AlertController() { }
     void InitHandlers() override;
+    /// Returns the requested alert(s).
+    /**
+     * HandleGet returns an alert or collection of alerts determined
+     * by the parameters in the URL path.
+     * If there is no URL path, this returns all alerts.
+     * If there is an alert id, or another search parameter, this returns
+     * all alerts that match the search key.
+     *
+     * @param message   The http request message to be parsed.
+     */
     void HandleGet(http_request message) override;
-    void HandlePut(http_request message) override;
+    /// Creates a new alert.
+    /**
+     * HandlePost adds a new alert based on information provided
+     * in the body of the http_request (in json format). This method
+     * forwards the message along for conversion to CMAC and CMSP distribution.
+     *
+     * @param message   The http request message that includes the alert info in
+     *                  json format with the body.
+     */
     void HandlePost(http_request message) override;
-    void HandleDelete(http_request message) override;
 private:
     DataAccessInterface& dao__;
 };
