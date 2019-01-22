4b4729f1b3eed6065c7f293086543b6a2327e7f8
vsagrawal0 <vaniya0@yahoo.com>
2019-01-21 18:38:23 -0600

    Document methods according to REST API specification.

--- rest-server/src/controller/cmsp_controller.hpp
+++ rest-server/src/controller/cmsp_controller.hpp
@@ -24,9 +24,49 @@ namespace cadg_rest {
         CMSPController(CMSPController& logger__, DataAccessInterface& dao__) : dao__(dao__), Controller(logger__) {}
         ~CMSPController() { }
         void InitHandlers() override;
+        /// Returns the CMSP, or collection of CMSPs, requested.
+        /**
+         * A plain GET request will return a collection of all CMSPs in
+         * the system. An extended URI will search on cmspID, while a
+         * query string will search on the keys passed (agency, name, scope).
+         * GET all CMSPs (/cmsps)
+         * GET specific CMSPs (/cmsps/{id})
+         * GET filtered CMSPs (/cmsps?name=SprintGW)
+         *
+         * @param message The http request message to be parsed.
+         */
         void HandleGet(http_request message) override;
+        /// Updates the specified CMSP with the provided information
+        /**
+         * A PUT request will specify in the URI the id of the CMSP being
+         * modified and send the information that needs to be updated. For example:
+         * {
+         *      name: updatedName,
+         *      agency: updatedAgency,
+         * }
+
+         * @param message   The http request message that contains the CMSP info
+         *                  in json format within the body.
+         */
         void HandlePut(http_request message) override;
+        /// Creates a new CMSP with the provided information
+        /**
+         * A POST request will include all the CMSP details. For example:
+         * {
+         *      name: SprintGW,
+         *      agency: Sprint Communications,
+         *      ...
+         * }
+         * @param message   The http request message that contains the CMSP info
+         *                  in json format within the body.
+         */
         void HandlePost(http_request message) override;
+        /// Deletes the specified CMSP
+        /**
+         * A DELETE request will specify in the URI, the id of the CMSP being deleted.
+         * The ID should be specified in the URL path.
+         * @param message   The http request message to be parsed.
+         */
         void HandleDelete(http_request message) override;
     private:
         DataAccessInterface& dao__;
