f8b78e2987654d8110d8f7a4a7cf5f6187d1e217
Ross <rosarc@gmail.com>
2019-01-25 20:36:59 -0600

    Implemented most of the CMAC node information
    - Some new structural questions have arisen, and will hopefully be addressed soon.

--- soap/cmac-conversion/cmac_converter.cpp
+++ soap/cmac-conversion/cmac_converter.cpp
@@ -4,11 +4,17 @@
 
 #include <iostream>
 #include <string>
+#include <ctime>
 #include "cmac_converter.hpp"
 #include "../pugixml-1.9/src/pugixml.hpp"
 #include "../pugixml-1.9/src/pugixml.cpp"
 
 void CMAC::convert(std::string soap_filename, std::string cmac_filename) {
+    //Obtaining the current date-time for later use.
+    std::time_t time = std::time(0);
+    std::tm* current_date_time = std::localtime(&time);
+    std::string date_time_str = std::to_string((current_date_time->tm_year + 1900)) + '-' + std::to_string((current_date_time->tm_mon + 1)) + '-' + std::to_string(current_date_time->tm_mday) + 'T' + std::to_string(current_date_time->tm_hour) + ':' + std::to_string(current_date_time->tm_min) + ':' + std::to_string(current_date_time->tm_sec) + 'Z';
+
     //Pulling the SOAP document into memory.
     pugi::xml_document soap_doc;
     pugi::xml_parse_result result = soap_doc.load_file(soap_filename.c_str());
@@ -88,6 +94,51 @@ void CMAC::convert(std::string soap_filename, std::string cmac_filename) {
 
     //Setting values for all CMAC document nodes.
     cmac_protocol_version.text().set("1.0");
+    cmac_sending_gateway_id.text().set(cap_message.child("sender").text().get()); //TODO: Determine how to get gateway IP?
+    cmac_message_number.text().set(cap_message.child("identifier").text().get()); //TODO: Determining how to identify CMSP-initiated value, when applicable.
+    cmac_special_handling.text().set(cap_message.child("scope").text().get()); //TODO: Ensure correctness.
+    cmac_sender.text().set(cap_message.child("sender").text().get());
+    cmac_sent_date_time.text().set(date_time_str.c_str());
+    cmac_status.text().set(cap_message.child("status").text().get());
+    cmac_message_type.text().set(cap_message.child("msgType").text().get());
+    cmac_response_code.text().set(cap_message.child("info").child("responseType").text().get());
+
+    if (cap_message.child("note")) {
+        cmac_note.text().set(cap_message.child("note").text().get());
+    }
+
+    cmac_cap_alert_uri.text().set("Temp value"); //TODO: Determine how to obtain from the gateway.
+    cmac_cap_identifier.text().set(cap_message.child("identifier").text().get());
+    cmac_cap_sent_date_time.text().set(cap_message.child("sent").text().get());
+
+    cmac_category.text().set(cap_message.child("info").child("category").text().get());
+
+    //TODO: Verify if this if-else is even necessary.
+    std::string special_handling = cmac_special_handling.text().get();
+    if (special_handling.compare("Presidential") == 0) {
+        cmac_event_code.text().set("EAN");
+    }
+    else if (special_handling.compare("Child Abduction") == 0) {
+        cmac_event_code.text().set("CAE");
+    }
+    else if (special_handling.compare("Required Monthly Test") == 0) {
+        cmac_event_code.text().set("RMT");
+    }
+
+    cmac_response_type.text().set(cap_message.child("info").child("responseType").text().get());
+    cmac_severity.text().set(cap_message.child("info").child("severity").text().get());
+    cmac_urgency.text().set(cap_message.child("info").child("urgency").text().get());
+    cmac_certainty.text().set(cap_message.child("info").child("certainty").text().get());
+    cmac_expires_date_time.text().set(cap_message.child("info").child("expires").text().get());
+    cmac_sender_name.text().set(cap_message.child("info").child("senderName").text().get());
+
+    if (cap_message.child("language")) {
+        cmac_text_language.text().set(cap_message.child("language").text().get());
+    }
+
+    std::string alert_message = cap_message.child("info").child("description").text().get();
+    cmac_text_alert_message_length.text().set(alert_message.length());
+    cmac_text_alert_message.text().set(cap_message.child("info").child("description").text().get());
 
     //Saving CMAC document.
     bool saved = cmac_doc.save_file(cmac_filename.c_str());
