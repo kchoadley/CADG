4d038b3a7942daeb8c3ee0551c49932603a4f11f
vsagrawal0 <vaniya0@yahoo.com>
2019-02-12 22:57:42 -0600

    Setup Redux fixtures (alerts, admins, aogs, & cmsps)

---
+++ cadg-ui/src/actions/cmspActions.js
@@ -0,0 +1,16 @@
+/// Defines actions that involve cmsp data.
+/**
+ * Defines the actions that can occur to manage cmsp data.
+ *
+ * Copyright 2018   Vaniya Agrawal, Ross Arcemont, Kristofer Hoadley,
+ *                  Shawn Hulce, Michael McCulley
+ *
+ * @file    cmspActions.js
+ * @date    February 2019
+ * @authors Vaniya Agrawal
+ */
+import {
+  GET_CMSPS,
+  CREATE_CMSP,
+  UPDATE_CMSP,
+  REMOVE_CMSP } from "./types";
---
+++ cadg-ui/src/actions/types.js
@@ -0,0 +1,19 @@
+export const GET_ALERTS = 'GET_ALERTS';
+export const SEND_ALERT = 'SEND_ALERT';
+export const UPDATE_ALERT = 'UPDATE_ALERT';
+export const CANCEL_ALERT = 'CANCEL_ALERT';
+
+export const GET_ADMINS = 'GET_ADMINS';
+export const CREATE_ADMIN = 'CREATE_ADMIN';
+export const UPDATE_ADMIN = 'UPDATE_ADMIN';
+export const REMOVE_ADMIN = 'REMOVE_ADMIN';
+
+export const GET_AOGS = 'GET_AOGS';
+export const CREATE_AOG = 'CREATE_AOG';
+export const UPDATE_AOG = 'UPDATE_AOG';
+export const REMOVE_AOG = 'REMOVE_AOG';
+
+export const GET_CMSPS = 'GET_CMSPS';
+export const CREATE_CMSP = 'CREATE_CMSP';
+export const UPDATE_CMSP = 'UPDATE_CMSP';
+export const REMOVE_CMSP = 'REMOVE_CMSP';
