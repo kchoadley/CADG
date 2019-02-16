20eb031315710f1f3f2772472c52b64ba0303266
vsagrawal0 <vaniya0@yahoo.com>
2019-02-16 09:19:42 -0600

    Fix dependency issue with Redux DevTools and make it optional.

--- cadg-ui/src/store.js
+++ cadg-ui/src/store.js
@@ -20,17 +20,26 @@ import rootReducer from './reducers';
 
 export const history = createHistory();
 const initialState = {};  // Represents the root of the store
+const enhancers = [];
 const middleware = [
   thunk,
   routerMiddleware(history)
 ]; // Specifies which middleware is being utilized
 
+if (process.env.NODE_ENV === 'development') {
+  const devToolsExtension =  window.__REDUX_DEVTOOLS_EXTENSION__;
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension());
+  }
+}
+
 const store = createStore(
   rootReducer(history),
   initialState,
   compose(
-    applyMiddleware(...middleware)//,
-   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+    applyMiddleware(...middleware),
+    ...enhancers
   )
 );
 
