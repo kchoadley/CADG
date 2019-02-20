579240784a8262e22679180f6b005a8f2cf3035c
vsagrawal0 <vaniya0@yahoo.com>
2019-02-20 12:35:52 -0600

    Restructure UI with Reactstrap

--- cadg-ui/src/App.js
+++ cadg-ui/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import Header from './components/header/header-content';
-import Main from './components/main/sidebar';
+import Header from './components/header/header';
+import Main from './components/main/main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Header/>
-          <Main/>
+        <Header/>
+        <Main/>
       </div>
     );
   }
