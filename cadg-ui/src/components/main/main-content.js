18bfe778dc279b6e5fc39353e81e084a2c1af92c
vsagrawal0 <vaniya0@yahoo.com>
2019-02-19 21:58:53 -0600

    UI shell created with Semantic UI React components

---
+++ cadg-ui/src/components/main/main-content.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../css/main-content.css'
+
+const Main = () => {
+  return (
+    <div className='full-height'>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+
+    </div>
+  );
+};
+
+export default Main;
