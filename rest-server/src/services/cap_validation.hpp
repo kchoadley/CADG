2cf443f7a9c84c496ce9df78ac547e75e15cd351
asumike <mbmccull@asu.edu>
2019-02-23 19:24:20 -0800

    Alert info resource validation

--- rest-server/src/services/cap_validation.hpp
+++ rest-server/src/services/cap_validation.hpp
@@ -95,55 +95,89 @@ namespace cap_validation {
         return true;
     }
 
-    static bool validate_soap_alert_info(const _ns2__alert_info alert_info) {
+    static bool validate_soap_alert_info_resource(const _ns2__alert_info_resource resource) {
+        try {
+            auto &logger(cadg_rest::Logger::Instance());
+
+            logger.Log(cadg_rest::LogLevel::DEBUG, "Validating required CAP info resource fields...", "cap_validation", "validate_soap_alert_info_resource");
+            // Validate required fields.
+            logger.Log(cadg_rest::LogLevel::DEBUG, "Info resource resourceDesc: " + resource.resourceDesc, "cap_validation", "validate_soap_alert_info_resource");
+            if (resource.resourceDesc == "" ) {
+                return false;
+            }
+            logger.Log(cadg_rest::LogLevel::DEBUG, "Info resource mimeType: " + resource.mimeType, "cap_validation", "validate_soap_alert_info_resource");
+            if (resource.mimeType == "" ) {
+                // TODO(All): Decide if this is required.
+                // This is required according to the documentation but missing from examples.
+                // The list of mime types appears to be extensive.
+                return false;
+            }
+            if (resource.uri) {
+                logger.Log(cadg_rest::LogLevel::DEBUG, "Info resource URI: " + *resource.uri, "cap_validation", "validate_soap_alert_info_resource");
+                // TODO(All): Validate URI format.
+                // Note: relative URI's are supported (to the derefUri element).
+            }
+            if (resource.derefUri) {
+                logger.Log(cadg_rest::LogLevel::DEBUG, "Info resource dereferenced URI: " + *resource.derefUri, "cap_validation", "validate_soap_alert_info_resource");
+                // TODO(All): Validate URI format.
+            }
+            return true;
+        } catch (...) {
+            return false;
+        }
+    }
+
+    static bool validate_soap_alert_info(const _ns2__alert_info info) {
         try {
             auto &logger(cadg_rest::Logger::Instance());
 
             logger.Log(cadg_rest::LogLevel::DEBUG, "Validating required CAP info fields...", "cap_validation", "validate_soap_alert_info");
             // Validate required fields.
-            if (alert_info.category.size() < 1) {
+            if (info.category.size() < 1) {
                 logger.Log(cadg_rest::LogLevel::DEBUG, "Info category missing.", "cap_validation", "validate_soap_alert_info");
                 return false;
             }
-            for (auto category = alert_info.category.begin(); category != alert_info.category.end(); ++category) {
+            for (auto category = info.category.begin(); category != info.category.end(); ++category) {
                 logger.Log(cadg_rest::LogLevel::DEBUG, "Info category: " + std::to_string(*category), "cap_validation", "validate_soap_alert_info");
                 if (*category < 0) {
                     return false;
                 }
             }
-            logger.Log(cadg_rest::LogLevel::DEBUG, "Info event: " + alert_info.event, "cap_validation", "validate_soap_alert_info");
-            if (alert_info.event == "") {
+            logger.Log(cadg_rest::LogLevel::DEBUG, "Info event: " + info.event, "cap_validation", "validate_soap_alert_info");
+            if (info.event == "") {
                 return false;
             }
-            logger.Log(cadg_rest::LogLevel::DEBUG, "Info urgency: " + std::to_string(alert_info.urgency), "cap_validation", "validate_soap_alert_info");
-            if (alert_info.urgency < 0) {
+            logger.Log(cadg_rest::LogLevel::DEBUG, "Info urgency: " + std::to_string(info.urgency), "cap_validation", "validate_soap_alert_info");
+            if (info.urgency < 0) {
                 return false;
             }
-            logger.Log(cadg_rest::LogLevel::DEBUG, "Info severity: " + std::to_string(alert_info.severity), "cap_validation", "validate_soap_alert_info");
-            if (alert_info.severity < 0) {
+            logger.Log(cadg_rest::LogLevel::DEBUG, "Info severity: " + std::to_string(info.severity), "cap_validation", "validate_soap_alert_info");
+            if (info.severity < 0) {
                 return false;
             }
-            logger.Log(cadg_rest::LogLevel::DEBUG, "Info certainty: " + std::to_string(alert_info.certainty), "cap_validation", "validate_soap_alert_info");
-            if (alert_info.certainty < 0) {
+            logger.Log(cadg_rest::LogLevel::DEBUG, "Info certainty: " + std::to_string(info.certainty), "cap_validation", "validate_soap_alert_info");
+            if (info.certainty < 0) {
                 // TODO(All): Modify generated gSoap object to recognize "Very Likely" as "Likely".
                 return false;
             }
-            if (alert_info.expires && *alert_info.expires > 0) {
-                // TODO(All): Alert.expires should change from a pointer to a required element if we update gSoap with IPAWS profile schema.
-                logger.Log(cadg_rest::LogLevel::DEBUG, "Info expires: " + *alert_info.expires, "cap_validation", "validate_soap_alert_info");
+            if (info.expires && *info.expires > 0) {
+                //TODO(All): Alert.expires should change from a pointer to a required element if we update gSoap with IPAWS profile schema.
+                logger.Log(cadg_rest::LogLevel::DEBUG, "Info expires: " + std::to_string(*info.expires), "cap_validation", "validate_soap_alert_info");
             } else {
                 logger.Log(cadg_rest::LogLevel::DEBUG, "Info expires missing.", "cap_validation", "validate_soap_alert_info");
                 return false;
             }
-            if (alert_info.eventCode.size() < 1) {
+
+            if (info.eventCode.size() < 1) {
                 logger.Log(cadg_rest::LogLevel::DEBUG, "Info eventCode missing.", "cap_validation",
                            "validate_soap_alert_info");
                 return false;
             }
             // TODO(All): Decide if SAME validation belongs here. Pertains to both eventCode and parameter.
             // The below requirement for eventCode with a valueName SAME is dependant upon intended dissemination type.
+            // We may want to allow these to pass validation, but then perform this check when sending outgoing messages.
             int same_event_code_count = 0;
-            for (auto event_code = alert_info.eventCode.begin(); event_code != alert_info.eventCode.end(); ++event_code) {
+            for (auto event_code = info.eventCode.begin(); event_code != info.eventCode.end(); ++event_code) {
                 logger.Log(cadg_rest::LogLevel::DEBUG, "Info eventCode valueName: " + (*event_code).valueName, "cap_validation", "validate_soap_alert_info");
                 logger.Log(cadg_rest::LogLevel::DEBUG, "Info eventCode value: " + (*event_code).value, "cap_validation", "validate_soap_alert_info");
                 if ((*event_code).valueName == "SAME") {
@@ -167,7 +201,7 @@ namespace cap_validation {
             }
 
             int same_parameter_count = 0;
-            for (auto parameter = alert_info.parameter.begin(); parameter != alert_info.parameter.end(); ++parameter) {
+            for (auto parameter = info.parameter.begin(); parameter != info.parameter.end(); ++parameter) {
                 logger.Log(cadg_rest::LogLevel::DEBUG, "Info parameter valueName: " + (*parameter).valueName, "cap_validation", "validate_soap_alert_info");
                 logger.Log(cadg_rest::LogLevel::DEBUG, "Info parameter value: " + (*parameter).value, "cap_validation", "validate_soap_alert_info");
                 if ((*parameter).valueName == "EAS-ORG") {
@@ -192,12 +226,18 @@ namespace cap_validation {
 
             logger.Log(cadg_rest::LogLevel::DEBUG, "Validating optional CAP info fields...", "cap_validation", "validate_soap_alert_info");
             // Validate optional fields.
-            if (alert_info.language) {
+            if (info.language) {
                 // TODO(All): Implement a language table per [RFC 3066] and validate this element.
             }
-            for (auto response_type = alert_info.responseType.begin(); response_type != alert_info.responseType.end(); ++response_type) {
+            for (auto response_type = info.responseType.begin(); response_type != info.responseType.end(); ++response_type) {
                 logger.Log(cadg_rest::LogLevel::DEBUG, "Info responseType: " + std::to_string(*response_type), "cap_validation", "validate_soap_alert_info");
                 if (*response_type < 0) {
+                    return false;
+                }
+            }
+
+            for (auto resource = info.resource.begin(); resource != info.resource.end(); ++resource) {
+                if (!validate_soap_alert_info_resource(*resource)) {
                     return false;
                 }
             }
@@ -284,11 +324,11 @@ namespace cap_validation {
             }
             // Validate alert info.
             logger.Log(cadg_rest::LogLevel::DEBUG, "Validating alert info.", "cap_validation", "validate_soap_alert");
-            std::vector<_ns2__alert_info> alert_infos = alert.info;
-            if (alert_infos.size() < 1)
+            std::vector<_ns2__alert_info> infos = alert.info;
+            if (infos.size() < 1)
                 return false;
-            for (auto alert_info = alert_infos.begin(); alert_info != alert_infos.end(); ++alert_info) {
-                if (!validate_soap_alert_info(*alert_info)) {
+            for (auto info = infos.begin(); info != infos.end(); ++info) {
+                if (!validate_soap_alert_info(*info)) {
                     return false;
                 }
             }
