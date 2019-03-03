6e47781d2b995951b8609332260755940ef98b65
Kristofer Hoadley <kristoferhoadley@gmail.com>
2019-03-03 13:24:41 -0600

    Updated doxygen for disseminator DAO interface.

--- rest-server/src/services/disseminator_dao_interface.hpp
+++ rest-server/src/services/disseminator_dao_interface.hpp
@@ -1,13 +1,13 @@
 // Copyright 2018   Vaniya Agrawal, Ross Arcemont, Kristofer Hoadley,
 //                  Shawn Hulce, Michael McCulley
-/// An interface for an Disseminator DAO.
+/// An interface for a Disseminator DAO.
 /**
  * This defines the interface for a data access object (DAO) which is
- * used to provide CRUD operations on a data storage of Alert Disseminators.
+ * used to provide CRUD operations on a data store of Disseminators.
  *
  * @file        disseminator_dao_interface.hpp
- * @authors     Michael McCulley
- * @date        January, 2019
+ * @authors     Michael McCulley, Kristofer Hoadley
+ * @date        March, 2019
  */
 #ifndef DISSEMINATOR_DAO_INTERFACE_H
 #define DISSEMINATOR_DAO_INTERFACE_H
@@ -17,51 +17,71 @@
 #include "disseminator.hpp"
 
 namespace cadg_rest {
+/// Interface defining what operations a disseminator data access object must implement.
 /**
  * DataAccessInterface defines several CRUD operations for a disseminator data store.
- * This interface must be defines in an inheriting class.
+ * This interface must be defined in an inheriting class.
  */
 class DisseminatorDaoInterface {
   public:
     /**
      * GetDisseminators gets all disseminators in the data store.
+     * If unsuccesful, the returned Optional will contain nothing.
      * 
-     * @return Vector of all disseminators.
+     * @return Optional Vector of all disseminators.
      */
     virtual std::optional<std::vector<Disseminator>> GetDisseminators() = 0;
     /**
      * GetDisseminatorsByName gets all disseminators in the data store which have a name that
      * partially or fully matches the name argument.
+     * If unsuccesful, the returned Optional will contain nothing.
+     * Returning nothing indicates an inability to connect to the data store.
      * 
      * @param name The name of the disseminator(s) to search for.
-     * @return Vector of all disseminators that match the name.
+     * @return Optional Vector of all disseminators that match the name.
      */
     virtual std::optional<std::vector<Disseminator>> GetDisseminatorsByName(const std::string& name) = 0;
     /**
      * GetDisseminatorByID gets the disseminator in the data store by id.
+     * If unsuccesful, the returned Optional will contain nothing.
+     * Returning nothing indicates an inability to connect to the data store.
      * 
      * @param id The id of the disseminator to return.
-     * @return Vector containing the found disseminator, or empty if there is no matching disseminator.
+     * @return Optional Vector containing the found disseminator, or empty if there is no matching disseminator.
      */
     virtual std::optional<std::vector<Disseminator>> GetDisseminatorByID(int id) = 0;
     /**
      * RemoveDisseminator removes the disseminator in the data store by id.
+     * If unsuccesful, the returned Optional will contain nothing.
+     * Returning nothing is different than returning false. False might indicate
+     * there is not a disseminator with the provided ID while nothing
+     * indicates an inability to connect to the data store.
      * 
      * @param id The id of the disseminator to remove.
-     * @return Bool indicating successful removal.
+     * @return Optional Bool indicating successful removal of disseminator.
      */
     virtual std::optional<bool> RemoveDisseminator(int id) = 0;
     /**
      * AddDisseminator adds the disseminator to the data store.
+     * If unsuccesful, the returned Optional will contain nothing.
+     * Returning nothing is different than returning false. False might indicate
+     * the provided disseminator is invalid or missing fields, while nothing
+     * indicates an inability to connect to the data store.
+     * 
      * 
      * @param disseminator The disseminator to add to the data store.
+     * @return Optional Bool indicating successful addition of disseminator.
      */
     virtual std::optional<bool> AddDisseminator(Disseminator disseminator) = 0;
     /**
      * UpdateDisseminator updates a disseminator in the data store.
+     * If unsuccesful, the returned Optional will contain nothing.
+     * Returning nothing is different than returning false. False might indicate
+     * the provided disseminator is invalid, missing fields, or a corresponding ID
+     * was not found, while nothing indicates an inability to connect to the data store.
      * 
-     * @param id The id of the disseminator to update.
-     * @param disseminator_info The updated information for the disseminator.
+     * @param disseminator The updated information for the disseminator.
+     * @return Optional Bool indicating a successful update of the disseminator.
      */
     virtual std::optional<bool> UpdateDisseminator(Disseminator disseminator) = 0;
 };
