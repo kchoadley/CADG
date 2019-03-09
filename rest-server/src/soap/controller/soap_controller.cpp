10ed221a7bc50ad71fe5953c391f03f560966b80
Ross <rosarc@gmail.com>
2019-03-09 14:04:01 -0600

    CAP message removal bandaid fix v.01

--- rest-server/src/soap/controller/soap_controller.cpp
+++ rest-server/src/soap/controller/soap_controller.cpp
@@ -11,7 +11,9 @@
 #include "../../gSoapFiles/CAP/soapH.h"
 #include "../../gSoapFiles/CAP/ns2.nsmap"
 #include "cap_validation.hpp"
-#include "cap_validation.hpp"
+#include "../../../../soap/pugixml-1.9/src/pugixml.hpp"
+#include "../../../../soap/pugixml-1.9/src/pugixml.cpp"
+
 using cadg_rest::LogLevel;
 
 namespace cadg_soap {
@@ -29,9 +31,21 @@ namespace cadg_soap {
             // Should generate soap context that can read input and create alert. Not sure how
             struct soap ctx = *soap_new2(SOAP_XML_STRICT, SOAP_XML_INDENT);
             auto body = message.extract_string().get();
+            pugi::xml_document soap_doc;
+            pugi::xml_parse_result result = soap_doc.load_string(body.c_str());
+            pugi::xml_node cap_message = soap_doc.first_child().first_child().first_child();
+            logger__.Log(LogLevel::DEBUG, "CAP node value: " + *cap_message.value(), "SoapController", "HandlePost");
+
+            //pugi::xml_document cap_doc;
+            //pugi::xml_node declaration_node = cap_doc.append_child(pugi::node_declaration);
+            //declaration_node.append_attribute("version") = "1.0";
+            //pugi::xml_node root_node = cap_doc.append_child(cap_message.value());
+            //std::stringstream pugi_str_stream;
+            //cap_doc.save(pugi_str_stream);
+
             logger__.Log(LogLevel::DEBUG, "SOAP Received: " + body, "SoapController", "HandlePost");
             std::stringstream str_stream;
-            str_stream.str(body);  // passes message body to into the string stream
+            str_stream.str(cap_message.value());  // passes message body to into the string stream
             ctx.is = &str_stream;  // sets the instream of the soap ctx  object to the string input stream
             // should read the soap context and output the details to the alertMessage object
             soap_read__ns2__alert(&ctx, &alertMessage);
