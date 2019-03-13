e7c7ea1127b53bbf1cba6df820054e98843e566f
SHulce <shawn_hulce@hotmail.com>
2019-03-12 22:10:35 -0500

    Beginning integration of gSOAP files.

--- soap-server/src/soap_server.cpp
+++ soap-server/src/soap_server.cpp
@@ -11,19 +11,24 @@
 #include <string>
 #include "log_level.hpp"
 #include "logger.hpp"
-#include "soapCAPSoapHTTPService.h"
+#include "gSOAPFiles/soapCAPSoapHTTPService.h"
+#include "gSOAPFiles/soapStub.h"
+#include "gSOAPFiles/soapH.h"
+#include "gSOAPFiles/stdsoap2.h"
+#include "gSOAPFiles/CAPSoapHttp.nsmap"
 using cadg_soap::LoggerInterface;
 using cadg_soap::LogLevel;
 using cadg_soap::Logger;
 
 int main(int argc, const char * argv[]) {
-    int port = 8000
-    if (argv == 1 ) {
+    int port = 8000;
+    if (std::strlen(*argv) == 1 ) {
         port = std::stoi(argv[0]);
     }
     LoggerInterface& logger(Logger::Instance());
     logger.LogLevel(LogLevel::DEBUG);
-    CAPSoapHTTPService service();
+    struct soap soap;
+    CAPSoapHttpService service(&soap);
 
     service.soap->send_timeout = service.soap->recv_timeout = 5; // 5 sec socket idle timeout
     service.soap->transfer_timeout = 30;                         // 30 sec message transfer timeout
@@ -32,4 +37,19 @@ int main(int argc, const char * argv[]) {
     service.destroy();
     return 0;
 
+}
+
+int CAPSoapHttpService::getRequest(ns2__requestParameterList *ns1__getRequestTypeDef,
+                                   ns3__responseParameterList &ns1__getResponseTypeDef) {
+    return SOAP_OK;
+}
+
+int CAPSoapHttpService::postCAP(_ns1__postCAPRequestTypeDef *ns1__postCAPRequestTypeDef,
+                                _ns1__postCAPResponseTypeDef &ns1__postCAPResponseTypeDef) {
+    return SOAP_OK;
+}
+
+int CAPSoapHttpService::getMessage(ns2__requestParameterList *ns1__getMessageTypeDef,
+                                   _ns1__messageResponseTypeDef &ns1__messageResponseTypeDef) {
+    return SOAP_OK;
 }
