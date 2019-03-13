952128315e095dea712db050acea6d38ae51c236
Shawn Hulce <shawn_hulce@hotmail.com>
2019-03-12 21:31:42 -0500

    GSOAP Files generated. Working on integration

--- soap-server/src/soap_server.cpp
+++ soap-server/src/soap_server.cpp
@@ -11,13 +11,25 @@
 #include <string>
 #include "log_level.hpp"
 #include "logger.hpp"
+#include "soapCAPSoapHTTPService.h"
 using cadg_soap::LoggerInterface;
 using cadg_soap::LogLevel;
 using cadg_soap::Logger;
 
 int main(int argc, const char * argv[]) {
+    int port = 8000
+    if (argv == 1 ) {
+        port = std::stoi(argv[0]);
+    }
     LoggerInterface& logger(Logger::Instance());
     logger.LogLevel(LogLevel::DEBUG);
-    logger.Log(LogLevel::DEBUG, "TESTING CADG SOAP COMPILATION");
+    CAPSoapHTTPService service();
+
+    service.soap->send_timeout = service.soap->recv_timeout = 5; // 5 sec socket idle timeout
+    service.soap->transfer_timeout = 30;                         // 30 sec message transfer timeout
+    while (service.run(port))
+        service.soap_stream_fault(std::cerr);
+    service.destroy();
     return 0;
+
 }
